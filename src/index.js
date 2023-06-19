f039ac9fea81e45db6fdafa277c2aa8ec9d146c0
IzacSilva <izaclislanio@gmail.com>
2023-06-19 04:43:18 -0300

    create footer menubar

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import './styles/css/main.css'
 import Header from './components/header/header.js';
 import Main from './components/main/main';
+import Footer from './components/footer/footer';
 import  App  from './App';
 
 const root = document.querySelector('#root')
 
 root.append(Header())
 root.append(Main())
+root.append(Footer())
 
